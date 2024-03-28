857a618ecf20d449afa4901382679fed526cf89f
SonZir0 <129615142+SonZir0@users.noreply.github.com>
2024-03-28 20:17:41 +0400

    Add listener to homeBtn, add style for current tab

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,26 @@ import './../css/style.css';
 import homeInit from './home.js';
 
 const contentDiv = document.querySelector('#content');
-homeInit(contentDiv);
+const homeBtn = document.querySelector('.homeBtn');
+
+homeBtn.addEventListener('click', () => {
+    if (homeBtn.classList[1] !== 'currentTab') {
+        clearContentDiv();
+        toggleChosenStyle(homeBtn);
+        homeInit(contentDiv);
+    }
+});
+
+// dispatch event to display Home tab on page load
+homeBtn.dispatchEvent(new MouseEvent("click"));
+
+function toggleChosenStyle(targetBtn) {
+    targetBtn.classList.toggle('currentTab');
+}
+
+function clearContentDiv() {
+    if (contentDiv.children.length) {
+        toggleChosenStyle(contentDiv.firstElementChild);
+        contentDiv.firstChild.remove();
+    }
+};
